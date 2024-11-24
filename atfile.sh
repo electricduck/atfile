461475a4aedae8029230e0141efbc86b0eb2ca7f
Ducky <ducky@zio.sh>
2024-11-24 00:53:55 +0000

    v0.6.7

--- atfile.sh
+++ atfile.sh
@@ -2679,7 +2679,7 @@ fi
 _prog="$(basename "$(atfile.util.get_realpath "$0")")"
 _prog_dir="$(dirname "$(atfile.util.get_realpath "$0")")"
 _prog_path="$(atfile.util.get_realpath "$0")"
-_version="0.6.6"
+_version="0.6.7"
 _command="$1"
 _command_full="$@"
 _dir_cache="$HOME/.cache/atfile"
