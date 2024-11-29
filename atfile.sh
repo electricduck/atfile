68b143671715f45c7660b2429efa8e51fcb4a592
Ducky <ducky@zio.sh>
2024-11-29 00:11:47 +0000

    help: fix text alignment

--- atfile.sh
+++ atfile.sh
@@ -2813,7 +2813,7 @@ function atfile.invoke.usage() {
     blob upload <path>
         Upload blobs to authenticated repository
         ℹ️  Unless referenced by a record shortly after uploading, blob will be
-          garbage collected by the PDS
+           garbage collected by the PDS
 
     handle <at-uri>
         Open at:// URI with relevant App
