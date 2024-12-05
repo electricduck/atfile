f7cd9e93954a6623bd5433210b938ba8211863d4
Ducky <ducky@zio.sh>
2024-12-05 12:53:51 +0000

    v0.8.5

--- atfile.sh
+++ atfile.sh
@@ -3212,7 +3212,7 @@ _meta_did="did:web:zio.sh" # @zio.sh
 _meta_repo="https://github.com/ziodotsh/atfile"
 _meta_year="2024"
 _now="$(atfile.util.get_date)"
-_version="0.8.4"
+_version="0.8.5"
 
 ### Reflection
 
