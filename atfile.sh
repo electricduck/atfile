4dc9b06620b322438ad462814c8cd21e828b8ba4
Ducky <ducky@zio.sh>
2024-12-05 00:43:21 +0000

    refactor auth into its own command
    This allows for re-authing with stuff sourcing ATFile

--- atfile.sh
+++ atfile.sh
@@ -1640,7 +1640,93 @@ function com.atproto.sync.uploadBlob() {
     atfile.xrpc.post_blob "$1" | jq -r ".blob"
 }
 
-# Commands
+# Main Functions
+
+function atfile.auth() {
+    override_username="$1"
+    override_password="$2"
+
+    [[ -n "$override_password" ]] && _password="$override_password"
+    [[ -n "$override_username" ]] && _username="$override_username"
+
+    if [[ $_is_git == 1 ]] && [[ $_command == "release" ]]; then
+        atfile.say.debug "Authenticating as '$_dist_username'..."
+
+        _fmt_blob_url="$_fmt_blob_url_default"
+        _password="$_dist_password"
+        _username="$_dist_username"
+    else
+        atfile.say.debug "Authenticating as '$_username'..."
+    fi
+
+    if [[ -z "$_server" ]]; then
+        skip_resolving=0
+        
+        if [[ $_is_sourced == 0 ]]; then
+            # NOTE: Speeds things up a little if the user is overriding actor
+            #       Keep this in-sync with the main command case below!
+            if [[ $_command == "cat" && -n "$3" ]] ||\
+            [[ $_command == "fetch" && -n "$3" ]] ||\
+            [[ $_command == "fetch-crypt" && -n "$3" ]] ||\
+            [[ $_command == "info" && -n "$2" ]] ||\
+            [[ $_command == "list" ]] && [[ "$2" == *.* || "$2" == did:* ]] ||\
+            [[ $_command == "list" && -n "$3" ]] ||\
+            [[ $_command == "url" && -n "$3" ]]; then
+                atfile.say.debug "Skipping identity resolving\n↳ Actor is overridden"
+                skip_resolving=1 
+            fi
+
+            # NOTE: Speeds things up a little if the command doesn't need actor resolving
+            if [[ $_command == "at:"* ]] ||\
+            [[ $_command == "atfile:"* ]] ||\
+            [[ $_command == "handle" ]] ||\
+            [[ $_command == "now" ]] ||\
+            [[ $_command == "resolve" ]] ||\
+            [[ $_command == "something-broke" ]]; then
+                atfile.say.debug "Skipping identity resolving\n↳ Not required for command '$_command'"
+                skip_resolving=1
+            fi
+        fi
+        
+        if [[ $skip_resolving == 0 ]]; then
+            atfile.say.debug "Resolving identity..."
+
+            resolved_did="$(atfile.util.resolve_identity "$_username")"
+            error="$(atfile.util.get_xrpc_error $? "$resolved_did")"
+            [[ -n "$error" ]] && atfile.die.xrpc_error "Unable to resolve '$_username'" "$resolved_did"
+
+            _username="$(echo $resolved_did | cut -d "|" -f 1)"
+            _server="$(echo $resolved_did | cut -d "|" -f 2)"
+            
+            atfile.say.debug "Resolved identity\n↳ DID: $_username\n↳ PDS: $_server"
+        fi
+    else
+        atfile.say.debug "Skipping identity resolving\n↳ ${_envvar_prefix}_ENDPOINT_PDS is set ($_server)"
+        [[ $_server != "http://"* ]] && [[ $_server != "https://"* ]] && _server="https://$_server"
+    fi
+
+    if [[ -n $_server ]]; then
+        if [[ $_skip_auth_check == 0 ]]; then
+            atfile.say.debug "Checking authentication is valid..."
+            
+            session="$(com.atproto.server.getSession)"
+            error="$(atfile.util.get_xrpc_error $? "$session")"
+
+            if [[ -n "$error" ]]; then
+                atfile.die.xrpc_error "Unable to authenticate" "$error"
+            else
+                _username="$(echo $session | jq -r ".did")"
+            fi
+        else
+            atfile.say.debug "Skipping checking authentication validity\n↳ ${_envvar_prefix}_SKIP_AUTH_CHECK is set ($_skip_auth_check)"
+            if [[ "$_username" != "did:"* ]]; then
+                atfile.die "Cannot skip authentication validation without a DID\n↳ \$${_envvar_prefix}_USERNAME currently set to '$_username' (need \"did:<type>:<key>\")"
+            fi
+        fi
+    fi
+}
+
+# Main Functions (To Refactor)
 
 function atfile.invoke.blob_list() {
     cursor="$1"
@@ -3332,87 +3418,14 @@ if [[ $_is_sourced == 0 ]]; then
     esac
 fi
 
-## Required variables detection
+## Authentication
 
-atfile.say.debug "Checking required variables..."
-[[ -z "$_username" ]] && atfile.die "\$${_envvar_prefix}_USERNAME not set"
-[[ -z "$_password" ]] && atfile.die "\$${_envvar_prefix}_PASSWORD not set"
-
-## Identity resolving
-
-if [[ $_is_git == 1 ]] && [[ $_command == "release" ]]; then
-    atfile.say.debug "Using release credentials..."
-
-    _fmt_blob_url="$_fmt_blob_url_default"
-    _password="$_dist_password"
-    _username="$_dist_username"
+if [[ $_is_sourced == 0 ]]; then
+    [[ -z "$_username" ]] && atfile.die "\$${_envvar_prefix}_USERNAME not set"
+    [[ -z "$_password" ]] && atfile.die "\$${_envvar_prefix}_PASSWORD not set"
 fi
 
-if [[ -z "$_server" ]]; then
-    skip_resolving=0
-    
-    if [[ $_is_sourced == 0 ]]; then
-        # NOTE: Speeds things up a little if the user is overriding actor
-        #       Keep this in-sync with the main command case below!
-        if [[ $_command == "cat" && -n "$3" ]] ||\
-           [[ $_command == "fetch" && -n "$3" ]] ||\
-           [[ $_command == "fetch-crypt" && -n "$3" ]] ||\
-           [[ $_command == "info" && -n "$2" ]] ||\
-           [[ $_command == "list" ]] && [[ "$2" == *.* || "$2" == did:* ]] ||\
-           [[ $_command == "list" && -n "$3" ]] ||\
-           [[ $_command == "url" && -n "$3" ]]; then
-            atfile.say.debug "Skipping identity resolving\n↳ Actor is overridden"
-            skip_resolving=1 
-        fi
-
-        # NOTE: Speeds things up a little if the command doesn't need actor resolving
-        if [[ $_command == "at:"* ]] ||\
-           [[ $_command == "atfile:"* ]] ||\
-           [[ $_command == "handle" ]] ||\
-           [[ $_command == "now" ]] ||\
-           [[ $_command == "resolve" ]] ||\
-           [[ $_command == "something-broke" ]]; then
-            atfile.say.debug "Skipping identity resolving\n↳ Not required for command '$_command'"
-            skip_resolving=1
-        fi
-    fi
-    
-    if [[ $skip_resolving == 0 ]]; then
-        atfile.say.debug "Resolving identity..."
-
-        resolved_did="$(atfile.util.resolve_identity "$_username")"
-        error="$(atfile.util.get_xrpc_error $? "$resolved_did")"
-        [[ -n "$error" ]] && atfile.die.xrpc_error "Unable to resolve '$_username'" "$resolved_did"
-
-        _username="$(echo $resolved_did | cut -d "|" -f 1)"
-        _server="$(echo $resolved_did | cut -d "|" -f 2)"
-        
-        atfile.say.debug "Resolved identity\n↳ DID: $_username\n↳ PDS: $_server"
-    fi
-else
-    atfile.say.debug "Skipping identity resolving\n↳ ${_envvar_prefix}_ENDPOINT_PDS is set ($_server)"
-    [[ $_server != "http://"* ]] && [[ $_server != "https://"* ]] && _server="https://$_server"
-fi
-
-if [[ -n $_server ]]; then
-    if [[ $_skip_auth_check == 0 ]]; then
-        atfile.say.debug "Checking authentication is valid..."
-        
-        session="$(com.atproto.server.getSession)"
-        error="$(atfile.util.get_xrpc_error $? "$session")"
-
-        if [[ -n "$error" ]]; then
-            atfile.die.xrpc_error "Unable to authenticate" "$error"
-        else
-            _username="$(echo $session | jq -r ".did")"
-        fi
-    else
-        atfile.say.debug "Skipping checking authentication validity\n↳ ${_envvar_prefix}_SKIP_AUTH_CHECK is set ($_skip_auth_check)"
-        if [[ "$_username" != "did:"* ]]; then
-            atfile.die "Cannot skip authentication validation without a DID\n↳ \$${_envvar_prefix}_USERNAME currently set to '$_username' (need \"did:<type>:<key>\")"
-        fi
-    fi
-fi
+atfile.auth
 
 ## Protocol handling
 
